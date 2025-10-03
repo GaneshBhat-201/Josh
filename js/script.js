38d1a0d67e0f2dd8935b454cd5821742d72054aa
Ganeshbhat <ganeshbhat777bs@gmail.com>
2025-10-03 14:56:39 +0530

    minor changes

--- js/script.js
+++ js/script.js
@@ -3,29 +3,24 @@ const dots = document.querySelectorAll(".dot");
 let currentIndex = 0;
 
 function showSlide(index) {
-  // move the slider
   const slider = document.querySelector(".rec-listing");
   slider.style.transform = `translateX(-${index * 365}px)`;
 
-  // update dots
   dots.forEach(dot => dot.classList.remove("active"));
   dots[index].classList.add("active");
 
   slides.forEach(slide => slide.classList.remove("active"));
   slides[index].classList.add("active");
 
-  // update current index
   currentIndex = index;
 }
 
-// Add click events for dots
 dots.forEach((dot, index) => {
   dot.addEventListener("click", () => {
     showSlide(index);
   });
 });
 
-// Auto-slide every 5 seconds
 setInterval(() => {
   let nextIndex = (currentIndex + 1) % slides.length;
   showSlide(nextIndex);
